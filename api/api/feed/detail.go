2c401e290c2b926bf9a21e271206014996cc6127
xmxx <xibaole2333@gmail.com>
2022-03-28 14:12:26 +0800

    test rewrites

--- api/api/feed/detail.go
+++ api/api/feed/detail.go
@@ -5,6 +5,7 @@ import (
 	coolapk "github.com/XiaoMengXinX/CoolapkApi-Go"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 func FeedDetail(w http.ResponseWriter, r *http.Request) {
@@ -17,12 +18,8 @@ func FeedDetail(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.WriteHeader(500)
 	}
+	w = writeHeader(result.Header, w, r)
 
-	for s, a := range result.Header {
-		for _, i := range a {
-			w.Header().Add(s, i)
-		}
-	}
 	_, _ = fmt.Fprintf(w, result.Response)
 }
 
@@ -31,4 +28,16 @@ func getArg(r *http.Request, name string) string {
 	values := r.URL.Query()
 	arg = values.Get(name)
 	return arg
+}
+
+func writeHeader(h http.Header, w http.ResponseWriter, r *http.Request) http.ResponseWriter {
+	for s, a := range h {
+		for _, i := range a {
+			if s == "Set-Cookie" {
+				s = strings.ReplaceAll(s, "coolapk.com", r.Host)
+			}
+			w.Header().Add(s, i)
+		}
+	}
+	return w
 }
--- api/api/feed/like.go
+++ api/api/feed/like.go
@@ -17,11 +17,7 @@ func FeedLike(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.WriteHeader(500)
 	}
+	w = writeHeader(result.Header, w, r)
 
-	for s, a := range result.Header {
-		for _, i := range a {
-			w.Header().Add(s, i)
-		}
-	}
 	_, _ = fmt.Fprintf(w, result.Response)
 }
