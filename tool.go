3d10e8fb81edcf1392bbe0fcacea1591a83d5b8e
XiaoMengXinX <xibaole2333@gmail.com>
2022-08-27 10:11:10 +0800

    Fix the specific recognition for this api x2

--- tool.go
+++ tool.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-const userAgentTmpl = `Dalvik/2.1.0 (Linux; U; Android %d) +CoolMarket/%d-%d-universal`
+const userAgentTmpl = `Dalvik/2.1.0 (Linux; U; Android %s; %s Build/%s; %s) (#Build; %s; %s; %s; %s) +CoolMarket/%s-%s-universal`
 
 var coolapkVersions = [][]string{
 	{"12.4.1", "2208081"},
@@ -17,11 +17,35 @@ var coolapkVersions = [][]string{
 	{"12.2.1", "2204291"},
 	{"12.2", "2204151"},
 }
-var androidVersions = []string{"6.0", "6.0.1", "7.0", "7.1.1", "7.1.2", "8.0", "8.1", "9", "10", "11", "12", "12"}
+var androidVersions = []string{"9", "10", "11", "12"}
+var models = []string{
+	"Pixel 3",
+	"Pixel 3 XL",
+	"Pixel 3a",
+	"Pixel 4",
+	"Pixel 4 XL",
+	"Pixel 4a",
+	"Pixel 5",
+	"Pixel 5a",
+}
+var buildNumbers = []string{
+	"SP2A.220505.002",
+	"SP2A.220405.003",
+	"SP2A.220305.012",
+	"SQ1A.220105.002",
+	"SQ1A.211205.008",
+	"SP1A.211105.004",
+	"RQ3A.211001.001",
+	"RQ3A.210905.001",
+	"RQ3A.210705.001",
+	"RP1A.200720.009",
+}
 
 func getRandomUA(tmpl string) string {
 	rand.Seed(time.Now().UnixNano())
 	androidVer := androidVersions[rand.Intn(len(androidVersions))]
 	coolapkVer := coolapkVersions[rand.Intn(len(coolapkVersions))]
-	return fmt.Sprintf(tmpl, androidVer, coolapkVer[0], coolapkVer[1])
+	model := models[rand.Intn(len(models))]
+	buildNumber := buildNumbers[rand.Intn(len(buildNumbers))]
+	return fmt.Sprintf(tmpl, androidVer, model, buildNumber, androidVer, "google", model, buildNumber, androidVer, coolapkVer[0], coolapkVer[1])
 }
